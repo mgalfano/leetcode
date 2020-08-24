3bd66888f485452478ec31ff4e4faf4da7412f94
Michelle <mysdmg312@gmail.com>
2020-08-24 14:11:29 -0700

    412. Fizz Buzz

---
+++ fizzBuzz.js
@@ -0,0 +1,40 @@
+/*
+412. Fizz Buzz
+
+Write a program that outputs the string representation of numbers from 1 to n.
+
+But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+*/
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+  // multiples of 3: print "Fizz"
+  // multiples of 5: print "Buzz"
+  // multiples of both 3 and 5: print "FizzBuzz"
+  // otherwise, print stringified number
+
+  var strings = [];
+  // loop from 1 to n
+  for (var i = 1; i <= n; i++) {
+    // if i % 3, "Fizz"
+    if (i % 3 === 0 && i % 5 === 0) {
+      strings.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      strings.push("Fizz");
+    } else if (i % 5 === 0){
+      strings.push("Buzz");
+    } else {
+      strings.push(`${i}`);
+    }
+    // if i % 5, "Buzz"
+    // if i % 3 and 5, "FizzBuzz"
+    // else, print "i" 
+  }
+  return strings;
+    
+};
+
+console.log(fizzBuzz(15));
